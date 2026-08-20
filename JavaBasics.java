ff5e2adad4d75a6dd9300f35a1eb10cd36c6073c
Anshumaan Tripathi <anshumaan.tripathi.in@gmail.com>
2026-08-20 21:20:05 +0530

    covered data types and implemented sum of two numbers

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,5 +1,6 @@
 public class JavaBasics {
   public static void main(String arg[]){
+    /*
     System.out.println("Hello Everyone!");
     System.out.println("Hello Everyone!");
     System.out.print("Hello Everyone!\n");
@@ -18,5 +19,27 @@ public class JavaBasics {
 
     a = b;
     System.out.println(a);
+    */
+
+
+   // Data types in java
+   // primitive - byte short char boolean int long float double
+   // non primitive - String Array Class Object Interface
+
+   // primitive
+   byte by = 8;
+   char ch = 'a';
+   boolean bool = true;
+   float price = 10.5f;
+   int num = 25;
+   long num2 = 955653333333364645L;
+   double num3 = 256.66;
+   short n = 240;
+
+   // sum of two  numbers
+   int a = 5;
+   int b = 15;
+   int sum = a + b;
+   System.out.println(sum);
   }
 }
