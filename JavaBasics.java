731ffdc3874986d5600b4ebe335f97a1d80fc871
Anshumaan Tripathi <anshumaan.tripathi.in@gmail.com>
2026-08-20 20:23:22 +0530

    started with java

---
+++ JavaBasics.java
@@ -0,0 +1,22 @@
+public class JavaBasics {
+  public static void main(String arg[]){
+    System.out.println("Hello Everyone!");
+    System.out.println("Hello Everyone!");
+    System.out.print("Hello Everyone!\n");
+
+    // variables
+    int a = 5;
+    int b = 10;
+    String name = "java";
+
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(name);
+
+    a = 15;
+    System.out.println(a);
+
+    a = b;
+    System.out.println(a);
+  }
+}
