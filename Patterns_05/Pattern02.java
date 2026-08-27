d0a4506df367288c1a9f60b3d270b6cc8859f004
Anshumaan Tripathi <anshumaan.tripathi.in@gmail.com>
2026-08-27 20:24:32 +0530

    INVERTED HALF-PYRAMID with Numbers pattern

--- Patterns_05/Pattern02.java
+++ Patterns_05/Pattern02.java
@@ -55,5 +55,19 @@ public class Pattern02 {
       }
         System.out.println();
     }
+
+    // INVERTED HALF-PYRAMID with Numbers pattern
+    // 1 2 3 4 5
+    // 1 2 3 4 
+    // 1 2 3
+    // 1 2 
+    // 1 
+
+    for(int i = 5; i > 0; i--) {
+      for(int j = 1; j<=i; j++) {
+        System.out.print(j);
+      }
+        System.out.println();
+    }
   }
 }
